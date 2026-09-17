07cd11f56c209d625ce7405fbe56f90f81c30ca2
nealgao <nealgao@163.com>
2026-09-17 16:36:20 +0800

    Add GitHub Pages site

---
+++ docs/_next/static/chunks/0ocy~1qxror~d.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,75097,o=>{"use strict";var t=o.i(88110);o.s(["default",0,function(){return(0,t.useEffect)(()=>{window.location.replace("/shanyao-portfolio/work/")},[]),null}])}]);
